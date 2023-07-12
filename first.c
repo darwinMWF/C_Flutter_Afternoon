05fb78a9f697e9b3a9d3e221f05ae17674cd4744
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-07-12 15:01:29 +0530

    First Lecture

---
+++ first.c
@@ -0,0 +1,22 @@
+#include<stdio.h>   
+int main()
+{
+//     float A=194.143;
+//     int a=12;
+//     int a=12;
+//     char c[] = "Atul";
+//     char C = 'A';
+  
+ 
+// printf("%.3f\n",A);
+// printf("%d\n",a);
+// printf("%d\n",a);
+// printf("%s\n",c);
+// printf("%c",C);
+
+char Name[50];
+printf("Enter Your Name:");
+scanf("%s",&Name);      //User Input
+printf("Your Name is :%s",Name);  //Output
+return 0;
+}
