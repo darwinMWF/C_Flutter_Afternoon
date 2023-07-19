91feb9d9710322f9c2c9dabe859219216e384955
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-07-19 16:00:44 +0530

    Loops

---
+++ fourth.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+
+    //  Single Line Comment
+    /*
+      Multiline Comment
+    */
+    // For Loop
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i==5)
+        {
+            printf("Five Was Here\n");
+           continue; //Jumping Statements
+            break;
+        }
+        else
+       printf("%d\n",i);
+    }
+
+    // while Loop
+    int i = 2;
+    while (i < 20)
+    {
+        printf("Hello\n");
+        i++;
+    }
+
+    
+    // Do While Loop
+    do
+    {
+       printf("Hello World\n");
+        i++;
+    } while (i>10);
+    
+
+    return 0;
+}
