b90c4c7e574ac1dc5daa615db62fb03e820bbedb
chirag_xd <kashidchirag2@gmail.com>
2023-07-26 16:16:24 +0530

    String

---
+++ string.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+
+// char a[] = {'a','b','c','d','f','f','f','f','d','f','f','f','d','f','d'};
+
+//   for (int i = 0; a[i]!='\0'; i++)
+//   {
+//    printf("%c\n",a[i]);
+//   }
+  
+//MultiLine String
+
+char name[50];
+
+
+printf("Enter Your Name:-");
+
+// gets(name);  //Old
+fgets(name,50,stdin);  //New
+// printf("Your Name is:-");
+puts(name);
+fputs(name,stdout);
+// scanf("%s",&name);
+// printf("Your Name Is:%s",name);
+
+
+    return 0;
+}
